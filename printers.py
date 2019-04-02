7932f2e81980104efe0b08ca569b0f77161bd8ea
plszymek221 <32601767+plszymek221@users.noreply.github.com>
2019-04-02 10:58:52 +0200

    Update printers.py

--- printers.py
+++ printers.py
@@ -1,76 +1,44 @@
 import csv
 class printer:
-	def __init__(self, model, typ, ile):
+	def __init__(self, model, type , quantity):
 		self.model = model
-		self.typ = typ
-		self.ile = ile
+		self.type = type
+		self.quantity = quantity
 	def info(self):
-		x = "Model: " + self.model + "; \t\t\tTyp: " + self.typ + "; \t\t\tIlosc: " + str(self.ile)
+		x = "Model: " + self.model + "; \t\t\tType: " + self.type + "; \t\t\tHow much: " + str(self.quantity)
 		return x
-	def wiecej(self):
-		self.ile += 1
-#	def ile():
+	def more(self):
+		self.quantity += 1
+
 line_counter = 0
-drukarki = []
+printers = []
 model=""
-historia=[]
+history=[]
 d=1
 f=0
 
-print('POWTARZAJACE SIE DRUKARKI\n\n')
+print('Repeating printers\n\n')
 with open('Pulpit/glpi.csv') as csvfile:
 	readCSV = csv.reader(csvfile, delimiter=';')
 	for row in readCSV:
 		model=row[5]
-		if model not in historia:
+		if model not in history:
 			if (line_counter == 0):
 				line_counter += 1
 			else:
-				drukarki.append(printer(row[5],row[4], d))
-				historia.append(row[5])
+				printers.append(printer(row[5],row[4], d))
+				history.append(row[5])
 				d=1
 				line_counter += 1
 		else:
 			line_counter += 1
 			x = line_counter - 2
 			m = row[5]
-			print('\t\t' + str(x) + '\t' + row[5] + '\t' + str(historia.index(row[5])))
-			drukarki[historia.index(row[5])].ile += 1
-
-
-
-
+			print('\t\t' + str(x) + '\t' + row[5] + '\t' + str(history.index(row[5])))
+			printers[history.index(row[5])].ile += 1
 
 print('\n\n\nDRUKARKI I ILE ICH JEST\n')
-
-
-#print(drukarki[0].info())
 c = 0
-for i in drukarki:
-	print(drukarki[c].info())
+for i in printers:
+	print(printers[c].info())
 	c+=1
-#nazwa = ""
-#nazwa += model
-#model = row[5]
-#typ = row[4]
-#nazwa = printer(model, typ)
-#modtyp[row[5]]=row[4]
-#modele.append(row[5])
-#line_counter += 1
-
-
-
-
-
-#Nazwa objektu - Model - row[5]
-#	Atrybuty obiektu
-#		Typ
-#			row[4]
-#		Potrzebne kolory
-#			jesli typ taki taki lub taki tyle kolorow
-#			jesli inny to tyle
-#		Ile razy sie powtarza
-#			func()
-#	Metody
-#		Na powtarzanie sie
-#
