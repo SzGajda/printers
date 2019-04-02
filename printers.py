5be44595aeb7737ce42dbe841233faec561ece02
plszymek221 <32601767+plszymek221@users.noreply.github.com>
2019-04-02 12:34:38 +0200

    Update printers.py

--- printers.py
+++ printers.py
@@ -1,11 +1,46 @@
 import csv
+final=[]
 class printer:
-	def __init__(self, model, type , quantity):
-		self.model = model
+	def __init__(self, type, model , quantity):
 		self.type = type
+		self.model = model
 		self.quantity = quantity
+		#USTALANIE BLACK/CMYK
+		if(self.type == "Igłowa"):
+			self.color = "Black"
+		elif(self.type == "Urządzenie laserowe monochromatyczne"):
+			self.color = "Black"
+		elif(self.type == "Urządzenie wielofunkcyjne monochromatyczne"):
+			self.color = "Black"
+		elif(self.type == "Urządzenie wielofunkcyjne kolorowe"):
+			self.color = "CMYK"
+		elif(self.type == "Urządzenie wielofunkcyjne kolorowe atramentowe"):
+			self.color = "CMYK"
+		else:
+			self.color = "Color"
+		#JEZELI BLACK TO BLACK I DODAJE 4 ELEMENTY DO LISTY OBIEKTOW
+		if(self.color == "Black"):
+			final.append(self.type)
+			final.append(self.model)
+			final.append(self.quantity)
+			final.append(self.color)
+		#JEZELI CMYK TO ROZBIJA NA 4 KOLORY I DODAJE 4*4=16 ELEMENTOW DO LISTY
+		else:
+			for i in range (4):
+				cmyk = ['Cyan', 'Magenta', 'Yellow', 'Black']
+				final.append(self.type)
+				final.append(self.model)
+				final.append(self.quantity)
+				final.append(cmyk[i])
+		#FUNKCJA POKAZUJACA WSZYSTKIE ATRYBUTY
 	def info(self):
-		x = "Model: " + self.model + "; \t\t\tType: " + self.type + "; \t\t\tHow much: " + str(self.quantity)
+		if(self.color=="Black"):
+			x = "Type: " + self.type + "; \tModel: " + self.model + "; \tHow many: " + str(self.quantity) + "; \tColor: " + self.color
+		else:
+			x = "Type: " + self.type + "; \tModel: " + self.model + "; \tHow many: " + str(self.quantity) + "; \tColor: Cyan"
+			x += "\nType: " + self.type + "; \tModel: " + self.model + "; \tHow many: " + str(self.quantity) + "; \tColor: Magenta"
+			x += "\nType: " + self.type + "; \tModel: " + self.model + "; \tHow many: " + str(self.quantity) + "; \tColor: Yellow"
+			x += "\nType: " + self.type + "; \tModel: " + self.model + "; \tHow many: " + str(self.quantity) + "; \tColor: Black"
 		return x
 	def more(self):
 		self.quantity += 1
@@ -17,7 +52,7 @@ history=[]
 d=1
 f=0
 
-print('Repeating printers\n\n')
+#print('Repeating printers\n\n')
 with open('Pulpit/glpi.csv') as csvfile:
 	readCSV = csv.reader(csvfile, delimiter=';')
 	for row in readCSV:
@@ -26,7 +61,7 @@ with open('Pulpit/glpi.csv') as csvfile:
 			if (line_counter == 0):
 				line_counter += 1
 			else:
-				printers.append(printer(row[5],row[4], d))
+				printers.append(printer(row[4],row[5], d))
 				history.append(row[5])
 				d=1
 				line_counter += 1
@@ -34,11 +69,12 @@ with open('Pulpit/glpi.csv') as csvfile:
 			line_counter += 1
 			x = line_counter - 2
 			m = row[5]
-			print('\t\t' + str(x) + '\t' + row[5] + '\t' + str(history.index(row[5])))
-			printers[history.index(row[5])].ile += 1
+			#print('\t\t' + str(x) + '\t' + row[5] + '\t' + str(history.index(row[5])))
+			printers[history.index(row[5])].quantity += 1
 
 print('\n\n\nDRUKARKI I ILE ICH JEST\n')
 c = 0
+
 for i in printers:
-	print(printers[c].info())
+	print(str(printers[c].info()))
 	c+=1
